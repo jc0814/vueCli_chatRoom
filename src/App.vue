bae287e1311d3d5facd7478ba6f56e884b87d961
jc0814 <0814.jessie@gmail.com>
2018-10-04 14:53:26 +0800

    1004 chatroom dev

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+             <router-link class="nav-link" :to ="'/'" >Hello</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" :to ="'Hello2'">Hello2</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+
+</style>
---
+++ src/components/Hello2.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="hello container mt-5">
+         <div class="input-group mb-5">
+            <input type="text" class="form-control" placeholder="Please your word.." v-model="message" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-outline-primary" @click="submitMessage" type="button" id="button-addon2">Send</button>
+            </div>
+        </div>
+        <div class="media my-3" v-for="item in messages">
+            <img v-if="item.username != username" class="align-self-start mr-3" src="http://lorempixel.com/100/100/sports" alt="Generic placeholder image">
+            <div class="media-body">
+                <h5 class="mt-0">{{item.username}}</h5>
+                <p>{{item.message}}</p>
+            </div>
+        <img v-if="item.username == username" class="ml-3" src="http://lorempixel.com/100/100/sports" alt="Generic placeholder image">
+        </div>
+       <!-- <div class="media">
+            <div class="media-body">
+                <h5 class="mt-0 mb-1">Media object</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+            <img class="ml-3" src="http://lorempixel.com/100/100/sports" alt="Generic placeholder image">
+        </div> -->
+    </div>
+</template>
+
+<script>
+  export default {
+      name:'item',
+      data(){
+          return {
+              msg: 'welcome to Your Vue.js App',
+              username:'JC Chin',
+              message:'',
+              messages:[{
+                username:'JC Chin',
+                message:'Hi sup!'
+              },
+              {
+                  username:'Iron Man',
+                  message:"Hey man! how's going?"
+              }
+              ]
+          }
+      },
+      methods:{
+          submitMessage(){ //equal to sumbitMessage: function (){}
+            let vm = this
+            vm.messages.unshift({ //let the message be put at front rahter than puting at last(by using 'push')
+                username:vm.username,
+                message: vm.message
+            })
+          }
+      }
+          
+     
+  }
+</script>
+
+<style>
+</style>
